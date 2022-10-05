44fa8fa51ec72e68b1d6e26f43198330440261ee
wveona <ms05114s@naver.com>
2022-10-05 12:52:01 +0900

    1

---
+++ note/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  note
+//
+//  Created by 박민서 on 2022/10/05.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
--- note/noteApp.swift
+++ note/noteApp.swift
@@ -13,7 +13,7 @@ struct noteApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
