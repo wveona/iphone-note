21fa8fc53de9cd8ddb01a26e34bcade4e90964a7
wveona <ms05114s@naver.com>
2022-10-06 16:07:06 +0900

    divide view

--- note/MainListView.swift
+++ note/MainListView.swift
@@ -13,15 +13,7 @@ struct MainListView: View {
     var body: some View {
         NavigationView {
             List(store.list){ memo in
-                VStack(alignment: .leading) {
-                    Text(memo.content)
-                        .font(.body)
-                        .lineLimit(1)
-                    
-                    Text(memo.insertDate, style: .date)
-                        .font(.caption)
-                        .foregroundColor(.secondary)
-                }
+                MemoCell(memo: memo)
             }
             .listStyle(.plain)
             .navigationTitle("내 메모")
@@ -36,3 +28,5 @@ struct MainListView_Previews: PreviewProvider {
             .environmentObject(MemoStore())
     }
 }
+
+
---
+++ note/View/MemoCell.swift
@@ -0,0 +1,30 @@
+//
+//  MemoCell.swift
+//  note
+//
+//  Created by 박민서 on 2022/10/06.
+//
+
+import SwiftUI
+
+struct MemoCell: View {
+    @ObservedObject var memo: Memo
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(memo.content)
+                .font(.body)
+                .lineLimit(1)
+            
+            Text(memo.insertDate, style: .date)
+                .font(.caption)
+                .foregroundColor(.secondary)
+        }
+    }
+}
+
+struct MemoCell_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoCell(memo: Memo(content: "Test"))
+    }
+}
